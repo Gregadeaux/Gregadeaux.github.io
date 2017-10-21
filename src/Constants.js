f1e3a2978ac0052afe43735774c30df1b201deaa
Greg Billetdeaux <greg@rokkincat.com>
2017-10-21 13:31:14 -0500

    Updated api route

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-// export const ROOT_URL = 'https://agile-dawn-31578.herokuapp.com'
-export const ROOT_URL = 'http://localhost:5000'
+export const ROOT_URL = 'https://agile-dawn-31578.herokuapp.com'
+// export const ROOT_URL = 'http://localhost:5000'
